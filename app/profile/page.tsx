b5aa568c17f21b2acacffe6a86186e23bf811013
SonSalem <salemmamdouh77777@gmail.com>
2025-01-22 05:22:35 +0200

    Fix Log Out

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,9 +5,11 @@ import Loader from "@/components/Loader";
 import { UserType } from "@/types";
 import axios from "axios";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const ProfilePage = () => {
+  const router = useRouter();
   const [userData, setUserData] = useState<UserType>();
   const [isLoading, setIsLoading] = useState<boolean>();
 
@@ -28,6 +30,11 @@ const ProfilePage = () => {
     fetchData();
   }, []);
 
+  const handleLogOut = () => {
+    localStorage.removeItem("userToken");
+    router.push("/login");
+  };
+
   return (
     <>
       <div
@@ -36,7 +43,7 @@ const ProfilePage = () => {
       >
         <div className="absolute top-0 left-0 w-full h-full bg-[#00000090]"></div>
       </div>
-      <div className="-mt-44 mb-44 relative z-10 flex flex-col md:flex-row gap-6 px-4 md:px-8 xl:px-40 dark:text-white items-start">
+      <div className="-mt-44 mb-44 relative z-10 flex flex-col md:flex-row justify-center items-center gap-6 px-4 md:px-8 xl:px-40 dark:text-white md:justify-start md:items-start">
         <div className="detailes bg-white dark:bg-bgDark-400 rounded-lg p-4 min-w-[280px] text-center shadow-md">
           {isLoading ? (
             <Loader />
@@ -49,7 +56,7 @@ const ProfilePage = () => {
                 height={80}
                 className="rounded-full mx-auto -mt-[calc(1rem+40px)] shadow-lg"
               />
-              <div className="data">
+              <div className="data mb-6">
                 <div className="text-2xl mb-3">#{userData?.username}</div>
                 <div className="flex gap-3 mb-2">
                   <span className="font-semibold">Name :</span>
@@ -77,6 +84,12 @@ const ProfilePage = () => {
                     {userData?.address.zipcode}
                   </span>
                 </div>
+              </div>
+              <div
+                onClick={handleLogOut}
+                className="cursor-pointer ring-1 ring-red-500 w-fit px-4 py-1 mx-auto rounded-sm text-red-500 hover:text-white hover:bg-red-500 transition-all duration-500"
+              >
+                Log Out
               </div>
             </>
           )}
